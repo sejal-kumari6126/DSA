17473cce5dec00be0dd0486886b650d0147ab0ea
Sejal Kumari <kumarisejal21@gmail.com>
2026-09-21 12:05:01 +0530

    Time: 0 ms (100%), Space: 43.3 MB (98.13%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i]);
+        }
+    }
+    public static void reverse(int row[]){
+        int l=0,r=row.length-1;
+        while(l<r){
+            int temp=row[l];
+            row[l]=row[r];
+            row[r]=temp;
+            l++;
+            r--;
+        }
+    }
+}
